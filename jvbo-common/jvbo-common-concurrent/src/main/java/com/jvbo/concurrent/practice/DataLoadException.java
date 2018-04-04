65d618a90f0d00476e66d96a1bc133d784673fd0
jvbo <1429834719@qq.com>
2018-04-05 02:52:21 +0800

    1. 一些同步工具类使用

---
+++ jvbo-common/jvbo-common-concurrent/src/main/java/com/jvbo/concurrent/practice/DataLoadException.java
@@ -0,0 +1,9 @@
+/*
+ * DataLoadException.java 2018年4月5日
+ * Copyright (c) 2015-2018, Jv Bo (programmer_jv_bo@163.com).
+ */
+package com.jvbo.concurrent.practice;
+
+public class DataLoadException extends Throwable {
+
+}
