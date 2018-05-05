5384a7a663b79c0bcff38f8c90fdce20b5dd9db4
jvbo <1429834719@qq.com>
2018-05-05 21:18:32 +0800

    1. some

---
+++ jvbo-common/jvbo-common-leetcode/src/main/java/com/jvbo/common/leetcode/JvboCommonLeetcodeApplication.java
@@ -0,0 +1,12 @@
+package com.jvbo.common.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvboCommonLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvboCommonLeetcodeApplication.class, args);
+	}
+}
