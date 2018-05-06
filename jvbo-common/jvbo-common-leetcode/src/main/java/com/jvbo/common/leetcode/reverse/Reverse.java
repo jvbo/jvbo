ab2efbe0b457714da1ef76fe056d0b7004dceb73
jvbo <1429834719@qq.com>
2018-05-06 22:09:53 +0800

    1. reverse str

---
+++ jvbo-common/jvbo-common-leetcode/src/main/java/com/jvbo/common/leetcode/reverse/Reverse.java
@@ -0,0 +1,25 @@
+/*
+ * Reverse.java 2018年5月6日
+ * Copyright (c) 2015-2018, Jv Bo (programmer_jv_bo@163.com).
+ */
+package com.jvbo.common.leetcode.reverse;
+
+/**
+ * 反转字符串
+ * @ClassName: Reverse 
+ * @Description: TODO
+ * @author jvbo
+ * @date 2018年5月6日
+ */
+public class Reverse {
+    public static void main(String[] args) {
+        String str="hello";
+        reverseStr(str);
+    }
+    
+    public static void reverseStr(String str){
+        String reverseStr = new StringBuffer(str).reverse().toString();
+        System.out.println("input:"+str);
+        System.out.println("output:"+reverseStr);
+    }
+}
