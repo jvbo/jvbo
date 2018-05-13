86d79f595182bc87d5d09cd2b4d3b02244006aed
jvbo <1429834719@qq.com>
2018-05-13 22:07:29 +0800

    1. cloud maven

--- jvbo-springcloud/jvbo-springboot-zuul/src/main/java/com/jvbo/springcloud/zuul/SpringCloudZuulApplication.java
+++ jvbo-springcloud/jvbo-springboot-zuul/src/main/java/com/jvbo/springcloud/zuul/SpringCloudZuulApplication.java
@@ -2,9 +2,24 @@ package com.jvbo.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableZuulProxy
+@EnableEurekaClient
+@ServletComponentScan
 @SpringBootApplication
 public class SpringCloudZuulApplication {
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZuulApplication.class, args);
